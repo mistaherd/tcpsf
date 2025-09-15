7f923b81330918bde93f276bc2a92c5201142e62
Mistaherd <lanzbulldog22@gmail.com>
2025-09-15 15:24:27 +0100

    innit

--- src/tcpsf.c
+++ src/tcpsf.c
@@ -28,6 +28,18 @@ int8_t get_command(char *arg){
    
    return Unknown;
 }
+void gethelp(int8_t arguement){
+   if (arguement ==Help){
+      printf("\r\ntcpsf (tcp send file ) will send a file across a network mainly the one i made myself the following are the commands : \r\n -h \t provides help on each command \r\n <filepath>\t If you pass in a filepath this will be the file sent to server/client \r\n -b \t if you have passed in a file as an aruguement this will  share the file in the local tcp network \r\n <ip adress> \t  another option from when you pass  the file path  is entering  in its  ip address ");
+   
+   }
+   else if(arguement==Filepath){
+      printf("\r\n at the moment  this command only supports csv,txt,png,jpeg and pdf files your option are :\r\n -b \t which broadcast the file to all devices in your tcp sever\r\n <ip address>\t will send that file to the ip address");
+   }
+   else{
+      printf("option not avilabe");
+   }
+}
 int8_t main(int8_t argc,char * argv[]){
    if (argc<2){
       printf("\n \033[31mError \33[0m: Usage %s <command>\n",argv[0]);
@@ -35,12 +47,12 @@ int8_t main(int8_t argc,char * argv[]){
    }
    switch (get_command(argv[1])){
       case Help:
-         printf("\r\ntcpsf (tcp send file ) will send a file across a network mainly the one i made myself the following are the commands : \r\n -h \t provides help on each command \r\n <filepath>\t If you pass in a filepath this will be the file sent to server/client \r\n -b \t if you have passed in a file as an aruguement this will  share the file in the local tcp network \r\n <ip adress> \t  another option from when you pass  the file path  is entering  in its  ip address ");
+         gethelp(Help);
          break;
       case Filepath:
          switch (get_command(argv[2])){
             case Help:
-               printf("\r\n at the moment  this command only supports csv,txt,png,jpeg and pdf files your option are :\r\n -b \t which broadcast the file to all devices in your tcp sever\r\n <ip address>\t will send that file to the ip address");
+               gethelp(Filepath);
                break;
             case Broadcast:
                printf("\r\nsending file to all devices in network");
